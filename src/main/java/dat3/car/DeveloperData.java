4c67af74fc3997fabd0cc74ca1d0951272e28f73
Rasmus2103 <rasm110c@stud.kea.dk>
2023-08-23 13:03:00 +0200

    Commit again

--- src/main/java/dat3/car/DeveloperData.java
+++ src/main/java/dat3/car/DeveloperData.java
@@ -1,2 +1,11 @@
-package dat3.car;public class DeveloperData {
+package dat3.car;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+
+public class DeveloperData implements ApplicationRunner {
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+
+    }
 }
